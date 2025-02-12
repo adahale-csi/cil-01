00c8d2286dd156968e4ae0c4c7011d49ef70868a
adahale-csi <ambarish.dahale@convergecfd.com>
2025-02-12 16:49:15 -0600

    Update generate_ct.py

--- generate_ct.py
+++ generate_ct.py
@@ -1,4 +1,4 @@
-data = ZEISSDataReader(filename).read()
+data = ZEISSDataReader(raw_x-ray_image-02.tif).read()
 data = TransmissionAbsorptionConverter()(data)
 recon = FDK(data).run()
 show2D(recon)
